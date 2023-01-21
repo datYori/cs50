703bc32c15877073fd5e0f5d6228591177ab2e29
Yann Bouzonie <yann.bouzonie@gmail.com>
2023-01-21 16:19:10 +0000

    better coms

--- mario.py
+++ mario.py
@@ -3,8 +3,9 @@
 """ A more clean way of implementing a mario pyramid """
 def pyramid_line(leftside, _i):
     """ A more clean way of implementing a mario pyramid """
-    # init list of size n-1 spaces char and 1 hash char at the end
+    # change last space in hash
     leftside[(len(leftside)-1)-_i] = '#'
+    # reverse list without modifiying original list
     rightside = leftside[::-1]
     return ''.join(leftside) + '  ' + ''.join(rightside)
 
--- wario.py
+++ wario.py
@@ -3,16 +3,14 @@
 """ Uggly way of implementing a mario pyramid """
 def main():
     """ One function to rule them all """
-    # init list of size 7 spaces char and 1 hash char at the end
+    # init
     _n = 8
     leftside = [' ']*_n + ['#']
     for i in range(_n):
         leftside[_n-i] = '#'
         # reverse list without modifiying original list
         rightside = leftside[::-1]
-        # change before last element into hash char
         print(''.join(leftside) + '  ' + ''.join(rightside))
-
 
 if __name__ == '__main__':
     main()
